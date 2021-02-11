f1e5b2703a03a03fc14b091901d7c40a0d759e44
MarthaGCL <marthacamposlopez@outlook.es>
2021-02-10 19:48:40 -0600

    Commit numero 3

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,4 +1,10 @@
+import { Clients } from '../clients/index';
+import { EventContext } from '@vtex/api';
+
 //node/event/liveUsersUpdate.ts
-export async function updateLiveUsers() {
-    console.log('EVENT HANDLER: received event')
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+    const liveUsersProduct = await ctx.clients.analytics.getLiveUsers()
+    console.log('LIVE USERS:', liveUsersProduct)
+    return true
   }
