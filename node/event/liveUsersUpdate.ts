f4ee057fff92c78ead5b878e4ae7e73beb0cbe70
MarthaGCL <marthacamposlopez@outlook.es>
2021-02-10 20:28:07 -0600

    Commit 04

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,10 +1,50 @@
-import { Clients } from '../clients/index';
-import { EventContext } from '@vtex/api';
+import { Clients } from '../clients/index'
+import { EventContext } from '@vtex/api'
+import { COURSE_ENTITY } from '../utils/constants'
 
 //node/event/liveUsersUpdate.ts
 
 export async function updateLiveUsers(ctx: EventContext<Clients>) {
-    const liveUsersProduct = await ctx.clients.analytics.getLiveUsers()
-    console.log('LIVE USERS:', liveUsersProduct)
+    const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
+    console.log('MOCKED LIVE USERS:', liveUsersProducts)
+    await Promise.all(
+      liveUsersProducts.map(async ({ slug, liveUsers }) => {
+        try {
+          const [savedProduct] = await ctx.clients.masterdata.searchDocuments<{
+            id: string
+            count: number
+            slug: string
+          }>({
+            dataEntity: COURSE_ENTITY,
+            fields: ['count', 'id', 'slug'],
+            pagination: {
+              page: 1,
+              pageSize: 1,
+            },
+            schema: 'v1',
+            where: `slug=${slug}`,
+          })
+  
+          console.log('SAVED PRODUCT', savedProduct)
+  
+          await ctx.clients.masterdata
+            .createOrUpdateEntireDocument({
+              dataEntity: COURSE_ENTITY,
+              fields: {
+                count: liveUsers,
+                slug,
+              },
+              id: savedProduct?.id,
+            })
+            .then((res) => {
+              console.log(res)
+              return res
+            })
+        } catch (e) {
+          console.log(`failed to update product ${slug}`)
+          console.log(e)
+        }
+      })
+    ) 
     return true
   }
